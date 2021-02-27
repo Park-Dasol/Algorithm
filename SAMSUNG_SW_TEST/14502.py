bc60a3a56750704cdd3cb0abf17ebf380d7d03fe
dryans1 <dryans@naver.com>
2021-02-27 20:08:54 +0900

    연구소

--- SAMSUNG_SW_TEST/14502.py
+++ SAMSUNG_SW_TEST/14502.py
@@ -1,42 +1,47 @@
 import pprint
 
-def get_wall(cnt):
+def get_wall(cnt, k):
     if cnt == 3:
         # pprint.pprint(matrix)
         virus_check()
+        # print(minVirus)
+        return
+    if k > N * M:
         return
     for i in range(N):
         for j in range(M):
             if matrix[i][j] == 0:
                 matrix[i][j] = 1
-                get_wall(cnt + 1)
+                get_wall(cnt + 1, k+ 1)
                 matrix[i][j] = 0
 
 
 
 def virus_check():
+    # pprint.pprint(matrix)
     global minVirus
     visit = [[0]* M for _ in range(N)]
     q = viruslist[:]
     cnt = 0
+
     while q :
         (y, x) = q.pop(0)
-        visit[y][x] = 1
-        cnt += 1
+        if visit[y][x] == 0:
+            visit[y][x] = 1
+            cnt += 1
+        else:
+            continue
         if cnt >= minVirus:
-            break
+            return minVirus
         for dir in range(4):
             newy = y + dy[dir]
             newx = x + dx[dir]
             if newx>= 0 and newx < M and newy >=0 and newy < N :
                 if matrix[newy][newx] == 0 and visit[newy][newx] == 0 :
                     q.append((newy, newx))
-    # print(minVirus)
     if cnt < minVirus :
+        # print(minVirus)
         minVirus = cnt
-        print(minVirus)
-        pprint.pprint(matrix)
-        pprint.pprint(visit)
 
 
 
@@ -60,8 +65,9 @@ for i in range(N):
             safelist.append([i,j])
         elif matrix[i][j] == 2:
             viruslist.append((i, j))
-minVirus = N * M - len(viruslist) - wall
-get_wall(0)
-print(minVirus)
-print(N*M - minVirus - wall - 3 + len(viruslist))
+# minVirus = N * M - len(viruslist) - wall
+minVirus = N * M
+get_wall(0, 0)
+# print(minVirus)
+print(N*M - minVirus - wall - 3)
 
