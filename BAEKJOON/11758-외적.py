0409e94be098c49c0ccf1cdbc540e34e0ef0ce79
dryans1 <dryans@naver.com>
2021-03-18 21:13:00 +0900

    11758 ccw

---
+++ BAEKJOON/11758-외적.py
@@ -0,0 +1,17 @@
+dot = [list(map(int, input().split())) for _ in range(3)]
+
+def ccw(p1, p2, p3):
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+    return (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)
+
+result = ccw(dot[0], dot[1], dot[2])
+
+if result > 0:
+    print(1)
+elif result < 0 :
+    print(-1)
+else:
+    print(0)
+
