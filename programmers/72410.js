fae60d00d63f184dca71a40948ff28aef4024966
dryans <dryans@naver.com>
2021-04-01 21:08:32 +0900

    72410 in progress

---
+++ programmers/72410.js
@@ -0,0 +1,28 @@
+function solution(new_id) {
+  var answer = '';
+
+  new_id.toLowerCase()
+
+
+
+  return answer;
+}
+
+
+function extra(words) {
+  const i = 0
+  while (i < words.length) {
+    i++
+    let a = words.charAt(i)
+    if ((typeof(a) === 'string') and  ) {
+      words.splice(i, 1)
+    }
+  }
+}
+
+// console.log(typeof(1))
+// console.log(typeof('a'))
+// console.log(typeof('.'))
+// if( typeof('a') === 'string') {
+//   console.log('same')
+// }
